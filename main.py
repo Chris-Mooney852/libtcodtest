71b141bc1bc7d1b8bedc5f5334e461ba0b34960b
chris <chris@mooney.tech>
2021-03-04 08:30:03 +1000

    basic entity system in place

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 import tcod
 
+from engine import Engine
+from input_handlers import EventHandler
+from entity import Entity
+
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
-    
-    player_x = int(screen_width/2)
-    player_y = int(screen_height/2)
 
     tileset = tcod.tileset.load_tilesheet(
         "assets/tiles.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+
+    event_handler = EventHandler()
+
+    player = Entity(int(screen_width/2), int(screen_height/2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width/2 - 5), int(screen_height/2), "@", (255, 255, 0))
+    entities = {player, npc}
+
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
@@ -23,14 +32,11 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
+            engine.render(console=root_console, context=context)
 
-            context.present(root_console)
+            events = tcod.event.wait()
 
-            for event in tcod.event.wait():
-                if event.type == "QUIT":
-                    raise SystemExit()
-
+            engine.handle_events(events)
 
 if __name__ == "__main__":
     main()
